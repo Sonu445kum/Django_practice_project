de327f73b3415092afdeec541af4b67ed8d7781a
Sonu445Kum <sonuroy1629@gmail.com>
2025-08-26 17:04:08 +0530

    I have Create Two Models BlogPost And Product

--- projectApp/models.py
+++ projectApp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    published = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+# Create Models for the Product 
+class Product(models.Model):
+    name=models.CharField(max_length=100)
+    descriptions=models.TextField()
+    price=models.DecimalField(max_digits=10,decimal_places=2)
+    stock=models.PositiveBigIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - ${self.price}"
